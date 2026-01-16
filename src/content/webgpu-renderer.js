3bddc978331279b74254b98f887a744e720b4bdb
Pavel Kotlyarov <pashok-kot-ru@ya.ru>
2026-01-16 13:50:48 +0300

    Fix mirroring(h-flip) simbol. New version 1.5.1

--- src/content/webgpu-renderer.js
+++ src/content/webgpu-renderer.js
@@ -84,12 +84,12 @@ export class WebGPURenderer {
    */
   setupGeometry() {
     const quad = new Float32Array([
-      -0.5, -0.5, 0, 1,
-      0.5, -0.5, 1, 1,
-      -0.5, 0.5, 0, 0,
-      -0.5, 0.5, 0, 0,
-      0.5, -0.5, 1, 1,
-      0.5, 0.5, 1, 0
+      -0.5, -0.5, 0, 0,
+      0.5, -0.5, 1, 0,
+      -0.5, 0.5, 0, 1,
+      -0.5, 0.5, 0, 1,
+      0.5, -0.5, 1, 0,
+      0.5, 0.5, 1, 1
     ]);
 
     this.quadBuffer = this.device.createBuffer({
